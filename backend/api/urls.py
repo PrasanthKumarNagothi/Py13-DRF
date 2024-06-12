88e886e46bb6bbe4e4acbceac3413387d7669754
Prasanth Kumar Nagothi <nprasanth4046@gmail.com>
2024-06-12 12:21:54 +0530

    Configuration to add employees

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('welcome', views.welcome, name="welcome"),
     path('get', views.getEmployee, name="getEmployee"),
+    path('add', views.addEmployee, name="addEmployee"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -10,9 +10,20 @@ def welcome(request):
     print(data)
     return Response('Welcome to Django Rest Framework')
 
+
 @api_view(['GET'])
 def getEmployee(request):
     employee = Employee.objects.all()
     print(employee)
     serializer = EmployeeSerializer(employee, many=True)
-    return Response(serializer.data) 
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def addEmployee(request):
+    newEmployee = request.data
+    serializer = EmployeeSerializer(data=newEmployee, many=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(f"Employee successfully added {serializer.data}")
+    return Response(serializer.errors)
